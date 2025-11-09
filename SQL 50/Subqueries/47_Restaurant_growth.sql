4c3862b7f9294dbefa55523020297d61e1cc3f19
Fareena S <81307492+FareenaS@users.noreply.github.com>
2025-11-09 20:37:51 +0530

    Create 47_Restaurant_growth.sql

---
+++ SQL 50/Subqueries/47_Restaurant_growth.sql
@@ -0,0 +1,98 @@
+/*
+Table: Customer
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| customer_id   | int     |
+| name          | varchar |
+| visited_on    | date    |
+| amount        | int     |
++---------------+---------+
+In SQL,(customer_id, visited_on) is the primary key for this table.
+This table contains data about customer transactions in a restaurant.
+visited_on is the date on which the customer with ID (customer_id) has visited the restaurant.
+amount is the total paid by a customer.
+ 
+
+You are the restaurant owner and you want to analyze a possible expansion (there will be at least one customer every day).
+
+Compute the moving average of how much the customer paid in a seven days window (i.e., current day + 6 days before). average_amount should be rounded to two decimal places.
+
+Return the result table ordered by visited_on in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customer table:
++-------------+--------------+--------------+-------------+
+| customer_id | name         | visited_on   | amount      |
++-------------+--------------+--------------+-------------+
+| 1           | Jhon         | 2019-01-01   | 100         |
+| 2           | Daniel       | 2019-01-02   | 110         |
+| 3           | Jade         | 2019-01-03   | 120         |
+| 4           | Khaled       | 2019-01-04   | 130         |
+| 5           | Winston      | 2019-01-05   | 110         | 
+| 6           | Elvis        | 2019-01-06   | 140         | 
+| 7           | Anna         | 2019-01-07   | 150         |
+| 8           | Maria        | 2019-01-08   | 80          |
+| 9           | Jaze         | 2019-01-09   | 110         | 
+| 1           | Jhon         | 2019-01-10   | 130         | 
+| 3           | Jade         | 2019-01-10   | 150         | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+--------------+----------------+
+| visited_on   | amount       | average_amount |
++--------------+--------------+----------------+
+| 2019-01-07   | 860          | 122.86         |
+| 2019-01-08   | 840          | 120            |
+| 2019-01-09   | 840          | 120            |
+| 2019-01-10   | 1000         | 142.86         |
++--------------+--------------+----------------+
+Explanation: 
+1st moving average from 2019-01-01 to 2019-01-07 has an average_amount of (100 + 110 + 120 + 130 + 110 + 140 + 150)/7 = 122.86
+2nd moving average from 2019-01-02 to 2019-01-08 has an average_amount of (110 + 120 + 130 + 110 + 140 + 150 + 80)/7 = 120
+3rd moving average from 2019-01-03 to 2019-01-09 has an average_amount of (120 + 130 + 110 + 140 + 150 + 80 + 110)/7 = 120
+4th moving average from 2019-01-04 to 2019-01-10 has an average_amount of (130 + 110 + 140 + 150 + 80 + 110 + 130 + 150)/7 = 142.86
+*/
+
+
+--Using CTE's
+--For handling duplicates when we have more than one entry for a single date
+WITH Daily_totals AS(
+SELECT visited_on
+,SUM(amount) AS daily_amount
+FROM Customer
+GROUP BY visited_on
+),
+--Calculate the rolling sum and average
+rolling_calculation as(
+SELECT *,
+SUM(daily_amount) OVER (ORDER BY visited_on 
+                    ROWS BETWEEN CURRENT ROW AND 6 FOLLOWING) AS rolling_sum,                  
+ROUND((SUM(daily_amount) OVER (ORDER BY visited_on 
+                    ROWS BETWEEN CURRENT ROW AND 6 FOLLOWING)/7.0),2) AS rolling_average,
+LEAD(visited_on,6) OVER(ORDER BY visited_on) AS end_date          
+FROM Daily_totals
+WHERE visited_on BETWEEN visited_on AND DATEADD(day,6,visited_on)
+)
+SELECT
+end_date as visited_on
+,rolling_sum AS amount
+,rolling_average AS average_amount
+FROM rolling_calculation
+WHERE end_date IS NOT NULL
+ORDER BY end_date
+
+
+--Using Group By directly
+select visited_on
+,sum(sum(amount)) over (order by visited_on rows between 6 preceding and current row) as amount
+,round(sum(sum(amount)) over (order by visited_on rows between 6 preceding and current row) / 7.0,2) as average_amount 
+from Customer
+group by visited_on
+ORDER BY visited_on OFFSET 6 ROWS 
