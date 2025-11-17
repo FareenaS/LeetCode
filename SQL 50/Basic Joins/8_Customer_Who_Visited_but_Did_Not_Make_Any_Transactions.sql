4d47e03ef549af28b07917e3d65e3c218eebc98a
Fareena S <81307492+FareenaS@users.noreply.github.com>
2025-11-17 22:30:38 +0530

    Update 8_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql

--- SQL 50/Basic Joins/8_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
+++ SQL 50/Basic Joins/8_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -74,6 +74,8 @@ Customer with id = 96 visited the mall once and did not make any transactions.
 As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
 */
 
+--Using left join + null is faster than subquery with not in
+
 --Using subquery
 SELECT V.customer_id,COUNT(V.customer_id) AS count_no_trans
 FROM Visits V
