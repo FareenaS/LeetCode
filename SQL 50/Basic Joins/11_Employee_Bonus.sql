1fb2455296d01f19b102f178be8b132a7b2efe2b
Fareena S <81307492+FareenaS@users.noreply.github.com>
2025-11-27 23:10:07 +0530

    Update 11_Employee_Bonus.sql

--- SQL 50/Basic Joins/11_Employee_Bonus.sql
+++ SQL 50/Basic Joins/11_Employee_Bonus.sql
@@ -63,12 +63,22 @@ Output:
 +------+-------+
 */
 
+/*
+Note:-
+* In the question they have mentioned they want records which satisfies either of two conditions, which means we have to use OR operator.
+* When we do a left join, we will have null as the empId and bonus for the employees who did not recieve any bonus in the Bonus table, that is y in the where condition we can use either bonus is null or empId is null condition, both will give the same answer
+*/
 
 --Query using only WHERE clause
 SELECT E.name, B.bonus
 FROM Employee E
 LEFT JOIN Bonus B ON B.empId=E.empId
 where B.bonus<1000 OR B.bonus IS NULL;
+
+SELECT E.name,B.bonus
+FROM Employee E
+LEFT JOIN Bonus B ON E.empId = B.empId 
+WHERE B.bonus < 1000 OR B.empId IS NULL
 
 
 --Query using scalar function(Takes more time due to a function call)
